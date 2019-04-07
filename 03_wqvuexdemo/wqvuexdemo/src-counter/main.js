40c8d5bf3765feb7aba63b82c279bfcf06d8f401
wuqingvika <526288022@qq.com>
2019-04-07 20:39:07 +0800

    vuex

---
+++ 03_wqvuexdemo/wqvuexdemo/src-counter/main.js
@@ -0,0 +1,16 @@
+/*
+入口js
+ */
+import Vue from 'vue'
+import Counter from './Counter.vue'
+import store from './store'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: {
+    Counter
+  },
+  template: '<Counter/>',
+  store // 注册上vuex的store: 所有组件对象都多一个属性$store
+})
